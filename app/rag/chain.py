3c768784d571310a950269a9e1cc4abd3d8ad267
ananth0901 <deshmukhananth0901@gmail.com>
2026-04-05 13:21:23 +0530

    Update chain.py

--- app/rag/chain.py
+++ app/rag/chain.py
@@ -1,1 +1,22 @@
+from langchain.chat_models import ChatOpenAI
 
+def generate_answer(query, docs, memory):
+    llm = ChatOpenAI(temperature=0)
+
+    context = "\n".join([doc.page_content for doc in docs])
+
+    prompt = f"""
+    You are an intelligent assistant.
+    
+    Context:
+    {context}
+    
+    Question:
+    {query}
+    
+    Answer clearly with sources.
+    """
+
+    response = llm.predict(prompt)
+
+    return response
